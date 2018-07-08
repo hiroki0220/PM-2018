e862e0176d5b4448cf824af3556edb683a6c315f
hiroki0220 <s1642093hd@s.chibakoudai.jp>
2018-07-08 13:25:35 +0900

    アカウント登録後直接ログイン

---
+++ account_add/login.php
@@ -0,0 +1,55 @@
+<?php
+
+try
+{
+$mail=$_POST['mail'];
+$pass=$_POST['pass'];
+
+$mail= htmlspecialchars($mail,ENT_QUOTES,'UTF-8');
+$pass = htmlspecialchars($pass,ENT_QUOTES,'UTF-8');
+
+
+$dbServer = '127.0.0.1';
+$dbName = 'yabukib';
+$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
+$dbUser = 'root';
+$dbPass = '';
+
+$db= new PDO($dsn,$dbUser,$dbPass);
+$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+$sql='SELECT name,id FROM customers WHERE mail=? AND password=?';
+$prepare=$db->prepare($sql);
+$data[]=$mail;
+$data[]=$pass;
+$prepare->execute($data);
+
+$db=null;
+
+$result=$prepare->fetch(PDO::FETCH_ASSOC);
+
+if($result==false)
+{
+	echo 'メールアドレスかパスワードが間違っています。<br />';
+    echo '<a href="
+"> 戻る</a>';
+}
+else
+{
+	session_start();
+	$_SESSION['login']=1;
+	$_SESSION['mail']=$mail;
+	$_SESSION['name']=$result['name'];
+	$_SESSION['id']=$result['id'];
+	header('Location:../top.php');
+	exit();
+}
+
+}
+catch(Exception $e)
+{
+	echo 'ただいま障害により大変ご迷惑をお掛けしております。';
+	exit();
+}
+
+?>
